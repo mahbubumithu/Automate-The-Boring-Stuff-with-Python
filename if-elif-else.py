4fbc5290084ee822b95198101e913025b7a7ab54
Mahbub-from-Zadid-Desktop <mahbubumithu@gmail.com>
2024-11-15 23:37:31 +0600

    add if-elif-else

---
+++ if-elif-else.py
@@ -0,0 +1,12 @@
+name = 'Mithu'
+age = 3000
+if name == 'Alice':
+    print('Hi, Alice')
+elif age < 12:
+    print('You are not Alice, Kiddo.')
+elif age>100:
+    print('You are not Alice, Grannie')
+elif age >2000:
+    print('Unlike you, Alice is not an undead, imprtal vampire.')
+else:
+    print('Your are neither Mithu or Sentu')
